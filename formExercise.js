a66376823bf58898a5f40cb147384a17ef80eddb
ShirYahav <shiryahav07@gmail.com>
2021-10-04 00:24:46 +0300

    goodie

--- formExercise.js
+++ formExercise.js
@@ -1,5 +1,3 @@
-//const errorInFields = document.querySelector('div.errorInFields');
-//const emptyfields = document.createElement('h3')
 const people = []
 const properties = [
     'id',
@@ -16,20 +14,38 @@ const headers = {
     hobby: 'hobby',
     gender: 'gender',
     ProfileUrl: 'profileUrl',
+    edit : 'edit',
+    deletePerson : 'delete'
 }
 
 function add() {
     event.preventDefault()//come back to the later
+    const errorInFields = document.querySelector('.errorInFields')
     const person = {}
     const form = document.querySelector('form')
     for (let property of properties) {
         person[property] = form.elements[property].value
     }
-    console.log(people)
-    people.push(person); //you can push only to an array!!
-    renderTable();
+    if(isSameId(people)){
+        errorInFields.innerHTML = "id already exist"
+        renderTable(false);
+    }else{
+        errorInFields.innerHTML = "";
+        console.log(people)
+        people.push(person);//you can push only to an array!!
+        renderTable();
+    }
 }
 
+function isSameId(arrOfobjects){
+    const form = document.querySelector('form')
+    const id = form.elements['id'].value
+    for(let i = 0; i<arrOfobjects.length; i++){
+        if(arrOfobjects[i].id === id){
+            return true
+        }
+    }
+}
 
 function createTd(text) {
     const td = document.createElement('td');
@@ -40,20 +56,21 @@ function createTd(text) {
 
 function createTr(obj) {
     const tr = document.createElement('tr');
-    for (let property in obj) {
+    for (let property in obj) { //in is both for array(index) and obj(property name)
         let td
         if (property === "profileUrl") {
             const img = createImg(obj[property])
             td = createTd('')
             td.appendChild(img)
-        }
-        else {
+            //td = createTd('<img src ="'+ obj[prop] +'">')
+            //<img src ="sdsf">
+        }else{
             td = createTd(obj[property]) //value of property
         }
         tr.appendChild(td)// td will be the child of tr 
     }
     return tr;
-}
+} //i have a situation where i want my td to be either img or text so : let td
 
 function createImg(src) {
     const img = document.createElement('img')
@@ -70,46 +87,77 @@ function createTable(arrOfobjects) {
     const table = document.createElement('table')
     const tableHeaders = createHeaders()
     table.appendChild(tableHeaders)
-    for (obj of arrOfobjects) {
-        const tr = createTr(obj)
-        const tdEdit = createTd("<button onclick='edit(\""+ obj.id +"\")'>Edit</button>")
-        tr.appendChild(tdEdit)
-        table.appendChild(tr)
+    for (obj of arrOfobjects) { //for of is not for obj!!!
+            const tr = createTr(obj)
+            const tdEdit = createTd("<button onclick=edit('"+ obj.id +"')>Edit</button>")
+            tr.appendChild(tdEdit)
+            const tdDelete = createTd("<button onclick=deletePerson('"+ obj.id +"'>Delete</button>")
+            tr.appendChild(tdDelete)
+            table.appendChild(tr)
     }
     return table
 }
 
-const updateButton = document.querySelector('div.updateButton')
-function edit(personId){
+function findPerson(id){ //lets find the person first how? by the id
+    for(let person of people){
+        if (person.id === id){
+            return person
+        }
+    }
+}
+
+ function loadPerson0Details(editPerson){ //now lets render person back to form
+    const form = document.querySelector('form')
+    for (let property of properties) {
+        form.elements[property].value = editPerson[property]
+    }
+}
+
+function edit(id){//now we put variable on the person we want to edit and load it to form
+    const editPerson = findPerson(id)
+    loadPerson0Details(editPerson)
+    //after we render person to form we want ro hide add and show update
+    const addButton = document.querySelector(".addButton")
+    const updateButton = document.querySelector(".updateButton")
+    addButton.classList.add('hidden')
+    updateButton.classList.remove('hidden')
+}
+
+function update(){
+    //we track the new input from the form
+    //for people track the person with the same id and then use obj ref to change it 
     event.preventDefault()
     const form = document.querySelector('form')
-    const addButton = document.querySelector('button.addButton')
-    addButton.classList.add('invert')
-    console.log(form.classList)
-    console.log(personId)
-    for (let person of people) {
-        if(personId === person.id){
-            for(property of person){
-                form.elements[property] = person[property]
+    const person = {}
+    for (let property of properties) {
+        person[property] = form.elements[property].value
+        for(let updatedPerson of people){
+            if(updatedPerson.id === person.id){
+                updatedPerson[property] = person[property] //object by refference
             }
         }
     }
-    updateButton.innerHTML = "<button class='update' onclick='update(" +personId+ ")'>update</button>"
-    form.appendChild(updateButton)
+    renderTable()
+    const addButton = document.querySelector(".addButton")
+    const updateButton = document.querySelector(".updateButton")
+    addButton.classList.remove('hidden')
+    updateButton.classList.add('hidden')
+}
+
+function deletePerson(person){//think about it
+   const findDeletePerson = findPerson(person.id)
+   for(let i in people){
+      if(people[i] === findDeletePerson){
+          people.splice(i,person)
+      }
+   }
+   renderTable()
 }
 
 function renderTable() {
-    const table = createTable(people)
+    const table = createTable(people) // i havw to send the arrOfobjects here!! (people)
     const container = document.querySelector('div.list')
     container.innerHTML = '' //reseting the table so it wont show headers again
     container.appendChild(table)
 }
-
- //renderTable(); ->you call the function in add button
- 
-//  all fields must be filled
-//  same id : this person is akready exist 
-//  edit button will allow us to update fieds 
-//  update button will update 
-//  delete person 
-//  different color every 
+//renderTable(); //if i already have a person so it will render
